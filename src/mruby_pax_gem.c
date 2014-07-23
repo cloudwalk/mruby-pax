280bea70cf099b517e64c7b3fef41ba14b340f38
Thiago Scalone <thiago@scalone.com.br>
2014-07-23 19:22:32 -0300

    Implement _pax_time in class Time.

--- src/mruby_pax_gem.c
+++ src/mruby_pax_gem.c
@@ -1,4 +1,7 @@
 #include "mruby.h"
+#include "mruby/value.h"
+#include "mruby/array.h"
+
 #include "posapi.h"
 #include "posapi_all.h"
 
@@ -15,12 +18,34 @@ mrb_sleep(mrb_state *mrb, mrb_value self)
   return mrb_fixnum_value(miliseconds);
 }
 
+mrb_value
+mrb__pax_time(mrb_state *mrb, mrb_value self)
+{
+  uchar time[7];
+  const char sTime[20];
+
+  memset(&time, 0, sizeof(time));
+  memset(&sTime, 0, sizeof(sTime));
+
+  GetTime(&time);
+
+  sprintf(sTime, "20%02X-%02X-%02X %02X:%02X:%02X", (int)time[0], (int)time[1], (int)time[2], (int)time[3], (int)time[4], (int)time[5]);
+
+  return mrb_str_new_cstr(mrb, sTime);
+}
+
+
 void
 mrb_mruby_pax_gem_init(mrb_state* mrb)
 {
   struct RClass *krn;
+  struct RClass *tc;
+
   krn = mrb->kernel_module;
+  tc  = mrb_class_get(mrb, "Time");
+
   mrb_define_method(mrb, krn, "__sleep__", mrb_sleep, MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb, tc, "_pax_time", mrb__pax_time, MRB_ARGS_NONE());
 }
 
 void
