82801c3d3cb537ce21eb99bd19482b23b7882976
Thiago Scalone <thiago@scalone.com.br>
2014-10-31 17:20:48 -0200

    Removed sleep(mruby-sleep) and time(mruby-time) implementation. With SDK Prolin is possible to work with canonical mrubygems.

--- src/mruby_pax_gem.c
+++ src/mruby_pax_gem.c
@@ -4,43 +4,8 @@
 #include "mruby/value.h"
 #include "mruby/array.h"
 
-#ifdef PAX
-#include "posapi.h"
-#include "posapi_all.h"
-#endif
 
 #define DONE mrb_gc_arena_restore(mrb, 0)
-
-mrb_value
-mrb_s___sleep__(mrb_state *mrb, mrb_value self)
-{
-  mrb_int miliseconds=0;
-  mrb_get_args(mrb, "i", &miliseconds);
-
-#ifdef PAX
-  DelayMs(miliseconds);
-#endif
-
-  return mrb_fixnum_value(miliseconds);
-}
-
-mrb_value
-mrb_s__pax_time(mrb_state *mrb, mrb_value self)
-{
-  char time[7];
-  const char sTime[20];
-
-  memset(&time, 0, sizeof(time));
-  memset(&sTime, 0, sizeof(sTime));
-
-#ifdef PAX
-  GetTime(&time);
-#endif
-
-  sprintf(sTime, "20%02X-%02X-%02X %02X:%02X:%02X", (int)time[0], (int)time[1], (int)time[2], (int)time[3], (int)time[4], (int)time[5]);
-
-  return mrb_str_new_cstr(mrb, sTime);
-}
 
 mrb_value
 mrb_s__serial(mrb_state *mrb, mrb_value self)
@@ -106,20 +71,13 @@ mrb_s__gets(mrb_state *mrb, mrb_value self)
 void
 mrb_mruby_pax_gem_init(mrb_state* mrb)
 {
-  struct RClass *krn;
-  struct RClass *tc;
   struct RClass *pax;
 
-  krn = mrb->kernel_module;
-  tc  = mrb_class_get(mrb, "Time");
   pax = mrb_define_class(mrb, "PAX", mrb->object_class);
 
-  mrb_define_method(mrb       , krn , "__sleep__"   , mrb_s___sleep__      , MRB_ARGS_REQ(1));
-  mrb_define_class_method(mrb , tc  , "_pax_time"   , mrb_s__pax_time      , MRB_ARGS_NONE());
   mrb_define_class_method(mrb , pax , "_serial"     , mrb_s__serial        , MRB_ARGS_NONE());
   mrb_define_class_method(mrb , pax , "_backlight=" , mrb_s__set_backlight , MRB_ARGS_REQ(1));
   mrb_define_class_method(mrb , pax , "_battery"    , mrb_s__battery       , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb , pax , "_gets"       , mrb_s__gets          , MRB_ARGS_REQ(2));
 }
 
 void
