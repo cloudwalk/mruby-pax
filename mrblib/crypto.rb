cd2b1a6120327ef9e554f26330e77e815a94fb04
danilo <danilo.maldito@gmail.com>
2015-07-21 16:16:03 -0300

    Added DUKPT_DEFAULT hash in order to return ksn and pinblock on get_pin_dukpt method.

--- mrblib/crypto.rb
+++ mrblib/crypto.rb
@@ -4,8 +4,17 @@ class PAX
     LOADKEY_DES         = 0
     LOADKEY_3DES        = 1
 
+    DUKPT_DEFAULT = {
+      "KSN"         => "",
+      "PINBLOCK"    => ""
+    }
+
     class << self
       attr_accessor :crypto
+    end
+
+    def self.dukpt_default
+      DUKPT_DEFAULT.dup
     end
   end
 end
