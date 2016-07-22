5f121296d752d28b13a6f3897a7da86b445b3e1b
Thiago Scalone <thiago@scalone.com.br>
2016-07-22 14:52:27 -0300

    Declare Pinpad::DES_DEFAULT and others constants to perform MK and DUKPT operations.

--- mrblib/pinpad.rb
+++ mrblib/pinpad.rb
@@ -1,12 +1,37 @@
 class PAX
 
   class Pinpad
+    RET_OK              = 0
     LOADKEY_DES         = 0
     LOADKEY_3DES        = 1
+
+    PED_INT_TMK         = 2
+    PED_INT_TLK         = 1
+    PED_INT_TDK         = 5
+    MODE_ECB_DECRYPTION = 0
+    MODE_ECB_ENCRYPTION = 1
+    MODE_CBC_DECRYPTION = 2
+    MODE_CBC_ENCRYPTION = 3
+    MODE_OFB_DECRYPTION = 4
+    MODE_OFB_ENCRYPTION = 5
+
+    TDK_SLOT            = 1
+    TWK_SLOT            = 2
+
+    PED_TMK             = "\x02"
+    PED_TLK             = "\x01"
+    PED_TDK             = "\x05"
+    MODE_DECRYPTION     = "\x00"
+    MODE_ENCRYPTION     = "\x01"
 
     DUKPT_DEFAULT = {
       "KSN"      => "",
       "PINBLOCK" => ""
+    }
+
+    DES_DEFAULT = {
+      "BLOCK"  => "",
+      "RETURN" => ""
     }
 
     class << self
@@ -15,6 +40,10 @@ class PAX
 
     def self.dukpt_default
       DUKPT_DEFAULT.dup
+    end
+
+    def self.des_default
+      DES_DEFAULT.dup
     end
   end
 end
