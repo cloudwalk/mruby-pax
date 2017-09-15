7e2b4be0747b33d65dee5a550326ff43bdb806e1
Thiago Scalone <thiago@scalone.com.br>
2017-09-15 02:25:32 -0300

    Refactoring Audio API

--- mrblib/audio.rb
+++ mrblib/audio.rb
@@ -1,12 +1,20 @@
 class PAX
   class Audio
     def self.volume=(value)
-      path = "/sys/devices/platform/keypad/buzzer_keypad_volume"
-      if File.exists? path
-        file = File.open("/sys/devices/platform/keypad/buzzer_keypad_volume", "w+")
-        file.write("#{value}\n")
-        file.close
+      @volume = value
+      if value == 0
+        PAX::System._os_set_value("persist.sys.sound.enable", "false")
+      else
+        path = "/sys/devices/platform/keypad/buzzer_keypad_volume"
+        if File.exists? path
+          File.open("/sys/devices/platform/keypad/buzzer_keypad_volume", "w") {|f| f.write("#{value}\n") }
+        end
       end
+    rescue IOError
+    end
+
+    def self.volume
+      @volume || 0
     end
   end
 end
