1461073b1089afc7806deb04c6ecb5e94eec339e
Thiago Scalone <thiago@scalone.com.br>
2017-11-07 16:41:50 -0200

    Touch Screen refactoring

--- src/touch.c
+++ src/touch.c
@@ -9,21 +9,24 @@
 
 #include "osal.h"
 #include "xui.h"
-#include "ts.h"
+#include "touch_screen.h"
 
 static mrb_value
 mrb_touch_s__getxy(mrb_state *mrb, mrb_value self)
 {
-  mrb_int x = 0, y = 0, ret = 0;
+  mrb_int x = 0, y = 0, ret = 0, timeout = 0;
   mrb_value hash;
 
-  ret = GetPixel(&x, &y);
+  mrb_get_args(mrb, "i", &timeout);
+
+  ret = GetTouchScreen(timeout, &x, &y);
 
   hash = mrb_hash_new(mrb);
-
-  mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "x")      , mrb_fixnum_value(x));
-  mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "y")      , mrb_fixnum_value(y));
-  mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "return") , mrb_fixnum_value(ret));
+  if (ret == 1) {
+    mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "x")      , mrb_fixnum_value(x));
+    mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "y")      , mrb_fixnum_value(y));
+    mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "return") , mrb_fixnum_value(ret));
+  }
 
   return hash;
 }
@@ -36,6 +39,6 @@ mrb_touch_init(mrb_state* mrb)
   pax   = mrb_class_get(mrb, "PAX");
   touch = mrb_define_class_under(mrb, pax, "Touch", mrb->object_class);
 
-  mrb_define_class_method(mrb , touch , "getxy" , mrb_touch_s__getxy , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , touch , "_getxy" , mrb_touch_s__getxy , MRB_ARGS_REQ(1));
 }
 
