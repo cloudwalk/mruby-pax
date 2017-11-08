35b66c95223ac6908de9863604648167c1b3b980
Thiago Scalone <thiago@scalone.com.br>
2017-11-08 18:08:13 -0200

    Set persist.sys.timezone.tz in hwclock

--- mrblib/time.rb
+++ mrblib/time.rb
@@ -1,6 +1,10 @@
 class Time
   def hwclock(timezone = nil)
-    PAX::System.hwclock(self.year, self.month, self.day, self.hour, self.min, self.sec)
+    ret = PAX::System.hwclock(self.year, self.month, self.day, self.hour, self.min, self.sec)
+    if ret == 0
+      PAX::System._os_set_value("persist.sys.timezone.tz", timezone || "+0300")
+    end
+    ret
   end
 end
 
