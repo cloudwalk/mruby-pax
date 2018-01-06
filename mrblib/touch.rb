b76823ff94f7d7a5bd8243b4ef7e2cf8d3dfb731
Thiago Scalone <thiago@scalone.com.br>
2018-01-06 00:57:37 -0200

    Add beep for success touch event

--- mrblib/touch.rb
+++ mrblib/touch.rb
@@ -3,6 +3,7 @@ class PAX
     def self.getxy(timeout = 10_000)
       hash = self._getxy(timeout)
       if hash["return"] == 1
+        PAX::Audio.beep(7, 60)
         [hash["x"], hash["y"]]
       end
     end
@@ -10,6 +11,7 @@ class PAX
     def self.getxy_stream(timeout = 10_000)
       hash = self._getxy_stream(timeout)
       if hash["return"] == 1
+        PAX::Audio.beep(7, 60)
         [hash["x"], hash["y"]]
       end
     end
