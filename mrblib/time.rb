f211b8e87db139d630eeac6e55b215cb582344c6
Thiago Scalone <thiago@scalone.com.br>
2018-05-22 14:29:13 -0700

    Fix timezone setting at Time.hwclock

--- mrblib/time.rb
+++ mrblib/time.rb
@@ -2,7 +2,8 @@ class Time
   def hwclock(timezone = nil)
     ret = PAX::System.hwclock(self.year, self.month, self.day, self.hour, self.min, self.sec)
     if ret == 0
-      PAX::System._os_set_value("persist.sys.timezone.tz", timezone || "+0300")
+      timezone = timezone.to_s.strip.empty? ? "-0300" : timezone
+      PAX::System._os_set_value("persist.sys.timezone.tz", timezone)
     end
     ret
   end
